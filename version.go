f9d62289ebfa64a60f77ab8652ae4a622cd15e67
samber <samber@users.noreply.github.com>
2024-09-09 17:19:40 +0000

    bump v1.3.1

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package slogbetterstack
 
 const name = "samber/slog-betterstack"
-const version = "VERSION" // replaced by .github/workflows/release.yml
+const version = "v1.3.1" // replaced by .github/workflows/release.yml
